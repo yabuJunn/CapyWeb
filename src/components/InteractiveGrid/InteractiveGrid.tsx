84a5e2d68f021d774320ce07f37f0ce8b62c21c4
yabuJunn <juancho04reyes@gmail.com>
2024-09-26 15:40:29 -0500

    feat(feature): Created placeholder of grid components
    Created placeholders divs for the componets of the dynamic grid to be placed
    
    By: Juan David

--- src/components/InteractiveGrid/InteractiveGrid.tsx
+++ src/components/InteractiveGrid/InteractiveGrid.tsx
@@ -3,7 +3,8 @@ import 'gridstack/dist/gridstack.min.css';
 import { GridStack } from 'gridstack';
 import { useEffect } from 'react';
 
-//
+//Import components
+import { Balance } from '../../components/TotalBalance/Balance'
 
 export const InteractiveGrid = () => {
     useEffect(() => {
@@ -13,11 +14,33 @@ export const InteractiveGrid = () => {
 
     return <>
         <div id='gridContainer' className="grid-stack">
-            <div className="grid-stack-item">
-                
+            <div className="grid-stack-item" gs-h="2" gs-w="4">
+                {/* <Balance></Balance> */}
+                Balance
             </div>
-            <div className="grid-stack-item" gs-w="6">
-                <div className="grid-stack-item-content">Item 2 wider</div>
+            <div className="grid-stack-item" gs-h="1" gs-w="3">
+                Ingresos Totales
+            </div>
+            <div className="grid-stack-item" gs-h="1" gs-w="3">
+                Gastos Totales
+            </div>
+            <div className="grid-stack-item" gs-h="2" gs-w="2">
+                Capy
+            </div>
+            <div className="grid-stack-item" gs-h="6" gs-w="3">
+                Tarjetas
+            </div>
+            <div className="grid-stack-item" gs-h="2" gs-w="5">
+                Resumen Gastos e Ingresos
+            </div>
+            <div className="grid-stack-item" gs-h="2" gs-w="4">
+                Ahorro
+            </div>
+            <div className="grid-stack-item" gs-h="2" gs-w="5">
+                Nivel del Usuario
+            </div>
+            <div className="grid-stack-item" gs-h="2" gs-w="4">
+                Misiones
             </div>
         </div>
     </>
