05c36bf4a060d786737f4e627fbf492dde678a13
shalom05 <salito0527@gmail.com>
2024-09-26 16:36:26 -0500

    feat(feature): Added router
    Created router on main.tsx, added router for landing, login, register and dashboard page
    
    By: Salome Ruiz

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,10 +1,16 @@
+import { useNavigate } from 'react-router-dom'
 import './LandingPage.css'
 
 export const LandingPage = () => {
+    const navigate = useNavigate()
+    const handleClick = () => {
+        navigate('/login')
+    }
+
     return <>
         <main className='page'>
-            <h1>Landing Page</h1>
-            
+            <h1 onClick={() => { handleClick() }}>Landing Page</h1>
+
             <div id='background'>
 
             </div>
--- src/pages/LogIn/LogIn.tsx
+++ src/pages/LogIn/LogIn.tsx
@@ -1,19 +1,19 @@
 import './LogIn.css';
 
-function LogIn({  }) {
+function LogIn() {
   return (
     <>
-     
-        <div className='LogIn'>
-        <h1>LogIn</h1> 
-        <h3>Correo electronico</h3> 
-        <input type='email'  placeholder='Correo'/>
-        <h3>Contraseña</h3> 
-        <input type='password'  placeholder='Contraseña'/>
-        <h3>No recuerdas tu contraseña</h3> 
-        <button>Aceptar</button> 
-        <h3>¿No tienes una cuenta? <button>Registrarme ahora</button></h3> 
-        </div>
+
+      <div className='LogIn'>
+        <h1>LogIn</h1>
+        <h3>Correo electronico</h3>
+        <input type='email' placeholder='Correo' />
+        <h3>Contraseña</h3>
+        <input type='password' placeholder='Contraseña' />
+        <h3>No recuerdas tu contraseña</h3>
+        <button>Aceptar</button>
+        <h3>¿No tienes una cuenta? <button>Registrarme ahora</button></h3>
+      </div>
 
     </>
   );
