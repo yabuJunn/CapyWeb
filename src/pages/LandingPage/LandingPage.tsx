faaa718dccce8ef4b014c7a8733f5ba9af17c9d9
shalom05 <salito0527@gmail.com>
2024-09-26 15:57:55 -0500

    feat(feature): Created LandingPage
    Initializated landing page basic html structure
    
    By: Salome Ruiz

---
+++ src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,13 @@
+import './LandingPage.css'
+
+export const LandingPage = () => {
+    return <>
+        <main className='page'>
+            <h1>Landing Page</h1>
+            
+            <div id='background'>
+
+            </div>
+        </main>
+    </>
+}
