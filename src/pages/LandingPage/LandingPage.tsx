f267f02f53355614b21bcdfe66da2d119921f805
yabuJunn <juancho04reyes@gmail.com>
2024-10-01 22:38:03 -0500

    feat(feature): added LandingPage component secondSection
    Added second section, an image of mockups of the app and an arrow down on the first section
    
    By: Juan David

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,7 +1,14 @@
 import './LandingPage.css'
 
+//Import components
+
 import { NavLanding } from '../../components/NavLanding/NavLanding'
 import { LandingFirstSectionButtons } from '../../components/LandingFirstSectionButtons/LandingFirstSectionButtons'
+
+//Import images
+
+import arrowDownWhite from '../../assets/svg/arrowDownWhite.svg'
+import landingSecondSectionMockups from '../../assets/png/mockupsCapyPc.jpg'
 
 export const LandingPage = () => {
     return <>
@@ -11,6 +18,11 @@ export const LandingPage = () => {
                 <h1>Lorem ipsum dolor sit amet, consectetur.</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis efficitur arcu libero, vel condimentum neque aliquam vitae. Duis a imperdiet eros. Pellentesque id ante ac velit porta hendrerit vel vel erat. Mauris sit amet lacus tortor.</p>
                 <LandingFirstSectionButtons></LandingFirstSectionButtons>
+                <img src={arrowDownWhite} alt="" id='arrowDownWhite'/>
+            </section>
+
+            <section id='secondSection'>
+                <img src={landingSecondSectionMockups} alt="" />
             </section>
 
             <div id='background'>
