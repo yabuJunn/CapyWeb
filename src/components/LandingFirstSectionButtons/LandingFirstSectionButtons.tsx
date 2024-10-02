41c4a8cfa6ce5ffb3638a82590f6fe2a12750afc
yabuJunn <juancho04reyes@gmail.com>
2024-10-01 22:01:30 -0500

    feat(feature): added LandingFirstSectionButtons commponent
    Created LandingFirstSectionButtons commponent and styles
    
    By: Juan David

---
+++ src/components/LandingFirstSectionButtons/LandingFirstSectionButtons.tsx
@@ -0,0 +1,11 @@
+
+import './LandingFirstSectionButtons.css'
+
+export const LandingFirstSectionButtons = () => {
+    return <>
+        <div id="LoginRegisterButtonsContainer">
+            <button id="createAccountButton">Create Account</button>
+            <button id="logInButton">Log in</button>
+        </div>
+    </>
+}
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,12 +1,16 @@
 import './LandingPage.css'
 
-import { NavLanding } from '../../components/NavLanding/navLanding'
+import { NavLanding } from '../../components/NavLanding/NavLanding'
+import { LandingFirstSectionButtons } from '../../components/LandingFirstSectionButtons/LandingFirstSectionButtons'
 
 export const LandingPage = () => {
     return <>
         <main className='page'>
             <section id='firstSection'>
                 <NavLanding></NavLanding>
+                <h1>Lorem ipsum dolor sit amet, consectetur.</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis efficitur arcu libero, vel condimentum neque aliquam vitae. Duis a imperdiet eros. Pellentesque id ante ac velit porta hendrerit vel vel erat. Mauris sit amet lacus tortor.</p>
+                <LandingFirstSectionButtons></LandingFirstSectionButtons>
             </section>
             <h1>Landing Page</h1>
 
