5dac8f1a3715284e46c76527370e498e14cb0881
shalom05 <salito0527@gmail.com>
2024-10-02 23:12:34 -0500

    feat(feature): Added component MissionItem
    Created new component MissionItem of Missions component
    
    By: Salomé Ruiz

---
+++ src/components/MissionItem/MissionItem.tsx
@@ -0,0 +1,78 @@
+import './MissionItem.css'
+
+import logoWhite from '../../assets/svg/logo/logoCapyMoneyWhite.svg'
+import logoBlack from '../../assets/svg/logo/logoCapyMoneyBlack.svg'
+
+interface MissionItemProps {
+    text: string,
+    gainAmount: string,
+    backgroundColor: string,
+    capyPointsDark: boolean,
+    isCompleted: boolean
+}
+
+export const MissionItem = ({ text, gainAmount, backgroundColor, capyPointsDark, isCompleted }: MissionItemProps) => {
+    if (isCompleted) {
+        if (!capyPointsDark) {
+            return <>
+                <div className='MissionItemContainer'>
+                    <p className='MissionDesc'>{text}</p>
+
+                    <div className='MissionCapyPoints ligth' style={{ backgroundColor: backgroundColor }}>
+                        <img src={logoWhite} alt="" />
+                        <div className='CapyPointsAmount' >
+                            <p className='Capypoints'>Completed</p>
+                            <p className='Gain'>+{gainAmount}</p>
+                        </div>
+                    </div>
+                </div>
+            </>
+        }
+
+        return <>
+            <div className='MissionItemContainer'>
+                <p className='MissionDesc'>{text}</p>
+
+                <div className='MissionCapyPoints' style={{ backgroundColor: backgroundColor }}>
+                    <img src={logoBlack} alt="" />
+                    <div className='CapyPointsAmount dark' >
+                        <p className='Capypoints'>Completed</p>
+                        <p className='Gain'>+{gainAmount}</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    } else {
+
+        if (!capyPointsDark) {
+            return <>
+                <div className='MissionItemContainer'>
+                    <p className='MissionDesc'>{text}</p>
+
+                    <div className='MissionCapyPoints ligth' style={{ backgroundColor: backgroundColor }}>
+                        <img src={logoWhite} alt="" />
+                        <div className='CapyPointsAmount' >
+                            <p className='Capypoints'>Capypoints</p>
+                            <p className='Gain'>+{gainAmount}</p>
+                        </div>
+                    </div>
+                </div>
+            </>
+        }
+
+        return <>
+            <div className='MissionItemContainer'>
+                <p className='MissionDesc'>{text}</p>
+
+                <div className='MissionCapyPoints' style={{ backgroundColor: backgroundColor }}>
+                    <img src={logoBlack} alt="" />
+                    <div className='CapyPointsAmount dark' >
+                        <p className='Capypoints'>Capypoints</p>
+                        <p className='Gain'>+{gainAmount}</p>
+                    </div>
+                </div>
+            </div>
+        </>
+
+    }
+}
--- src/components/Missions/Missions.tsx
+++ src/components/Missions/Missions.tsx
@@ -1,4 +1,6 @@
-import './Missions.css';
+import './missions.css';
+
+import { MissionItem } from '../MissionItem/MissionItem';
 
 //Imagenes importadas
 import MoveButton from '../../assets/svg/MoveButton.svg'
@@ -6,11 +8,15 @@ import MoveButton from '../../assets/svg/MoveButton.svg'
 export const Missions = () => {
   return (
     <>
-      <div className='missions'> 
-      <img className="MoveButton" src={MoveButton} alt="MoveButton" />
-      <div className="info-container">
-          <h3>Misiones</h3> 
-      </div>
+      <div className='missions'>
+        <img className="MoveButton" src={MoveButton} alt="MoveButton" />
+        <div className="info-container">
+          <h3>Misiones</h3>
+          <MissionItem text={'Cumple todas las misiones mensuales.'} gainAmount={'500'} backgroundColor={'#2D18BF'} capyPointsDark={false} isCompleted={false}></MissionItem>
+          <MissionItem text={'Ahorra al menos un 35% de tus ingresos este mes.'} gainAmount={'50'} backgroundColor={'#F2622E'} capyPointsDark={false} isCompleted={true}></MissionItem>
+          <MissionItem text={'Cumple al menos con el 60%  de metas establecidas.'} gainAmount={'70'} backgroundColor={'#A8F25D'} capyPointsDark={true} isCompleted={false}></MissionItem>
+
+        </div>
       </div>
     </>
   );
