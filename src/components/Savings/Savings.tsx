4912b7850ba9f82575a36d7e01d5f79c4f9b2e0a
shalom05 <salito0527@gmail.com>
2024-10-03 00:20:47 -0500

    style(feat): Css Card Carro
    Css de una card que hacía falta
    
    by: Salomé Ruiz

--- src/components/Savings/Savings.tsx
+++ src/components/Savings/Savings.tsx
@@ -1,16 +1,39 @@
-import './Savings.css';
+import'./Savings.css';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
-//Imagenes importadas
-import MoveButton from '../../assets/svg/MoveButton.svg'
+import MoveButton from '../../assets/svg/MoveButton.svg';
 
 export const Savings = () => {
+  const ingresos = 78;
+
   return (
     <>
       <div className='savings'> 
-      <img className="MoveButton" src={MoveButton} alt="MoveButton" />
-      <p>Ahorro</p>
-      </div>
+        <img className="MoveButton" src={MoveButton} alt="MoveButton" />
+
+        <div className="progress-wrapper">
+          {/* Círculo de Ingresos */}
+          <CircularProgressbar
+            value={ingresos}
+            text={`Ingresos ${ingresos}%`}
+            styles={buildStyles({
+              rotation: 0.25, 
+              strokeLinecap: 'butt',
+              pathTransitionDuration: 0.5,
+              pathColor: ' #2D18BF',
+              textColor: '#fff',
+              trailColor: '#A8F25D', 
+              textSize: '13px',
+            })}
+          />
+          <p>ingresos:   78%</p>
+          <p>Ahorro:  22%</p>
+        
+          </div>
+        </div>
     </>
   );
 }
+
 
