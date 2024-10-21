63eaca1e7120361e79e86e0628e25ae673fde161
salazar2356 <isasalazar2356@gmail.com>
2024-10-20 21:48:17 -0500

    Feature: Created 2 graphic components,Expense and PieChart graphics

--- src/pages/IncomeAndIncrease/ExpensesAndIncome.tsx
+++ src/pages/IncomeAndIncrease/ExpensesAndIncome.tsx
@@ -1,10 +1,13 @@
 import { ExpensesGraphic } from "../../components/dashboardPageComponents/expensesAndIncomeScreenComponents/ExpensesGraphic/ExpensesGraphic";
+import { PieChartGraphic } from "../../components/dashboardPageComponents/expensesAndIncomeScreenComponents/ExpensesGraphic/PieChart/PieChartGraphic";
+
 
 export const ExpensesAndIncome = () => {
   return (
     <div className='page'>
         <h1>Income and Increase</h1>
         <ExpensesGraphic />
+        <PieChartGraphic />
  
     </div>
   );
