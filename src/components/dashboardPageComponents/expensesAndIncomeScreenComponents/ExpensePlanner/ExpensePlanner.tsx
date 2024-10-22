731feeeb5c279304b99b06ec8af01b5b04e34382
salazar2356 <isasalazar2356@gmail.com>
2024-10-22 15:21:19 -0500

    Feature: Added PopUp to ExpensePlanner component

--- src/components/dashboardPageComponents/expensesAndIncomeScreenComponents/ExpensePlanner/ExpensePlanner.tsx
+++ src/components/dashboardPageComponents/expensesAndIncomeScreenComponents/ExpensePlanner/ExpensePlanner.tsx
@@ -1,16 +1,47 @@
+import React, { useState } from 'react';
 import PlannerIcon from "../../../../../src/assets/svg/PlannerIcon.svg"
 
 export const ExpensePlanner = () => {
+
+ const [showPopup, setShowPopup ] = useState(false) 
+
+
+const togglePopUp = () =>{
+  setShowPopup(!showPopup)
+
+}
   return (
     <div className='planner'>
         <h1>Monthly expense planner</h1>
         <img className="PlannerIcon" src={PlannerIcon} alt="PlannerIcon" />
         <h3>You don't have budgeted expenses</h3>
-        <button>Add a new expense</button>
+        <button onClick={togglePopUp}>Add a new expense</button>
 
+        {(showPopup && (
+          <div>
+            <h2>New Expense</h2>
+            <h3>Tarjetas</h3>
+            <select name="tarjetas" required>
+            <option value="" disabled selected>Tarjetas</option> 
+            <option value="nu">Nu</option> 
+            <option value="visa">Visa</option> 
+            <option value="falabella">Falabella</option> 
+            <option value="nequi">Nequi</option> 
+            </select>
 
-       
- 
+            <h3>Presupuesto</h3>
+            <input type='number' />
+
+            <h3>Categporías</h3>
+            <select name="categories" required>
+            <option value="" disabled selected>Categoría</option> 
+            <option value={"hogar"}>Hogar</option>
+            <option value={"mercado"}>Mercado</option>
+            <option value={"ropa"}>Ropa</option>
+            </select>
+
+          </div>
+        ))}
     </div>
   );
 }
