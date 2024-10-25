359f6d3f75f71da34069450cacc2373ca13b4630
DANDIERU <1193080635@u.icesi.edu.co>
2024-10-25 17:52:02 -0500

    update grid created

--- src/pages/IncomeAndIncrease/ExpensesAndIncome.tsx
+++ src/pages/IncomeAndIncrease/ExpensesAndIncome.tsx
@@ -3,17 +3,20 @@ import { ExpensesGraphic } from "../../components/dashboardPageComponents/expens
 import { PieChartGraphic } from "../../components/dashboardPageComponents/expensesAndIncomeScreenComponents/PieChart/PieChartGraphic";
 import { History } from "../../components/dashboardPageComponents/expensesAndIncomeScreenComponents/History/History";
 
-
 export const ExpensesAndIncome = () => {
   return (
-    <div className='page'>
-        <h1>Income and Increase</h1>
-        <ExpensesGraphic />
-        <PieChartGraphic />
-        <ExpensePlanner />
-        <History />
+    <div className="page">
+      <h1>Income and Increase</h1>
+      <ExpensesGraphic />
+      <PieChartGraphic />
+      <ExpensePlanner />
+      <History />
 
- 
+      <div className="container">
+        <div className="main-div">Hola</div>
+        <div className="right-div">Hola</div>
+        <div className="left-div">Hola</div>
+      </div>
     </div>
   );
-}
+};
