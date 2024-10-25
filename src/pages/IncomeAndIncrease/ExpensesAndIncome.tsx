468eef7aff181628ec9f334229f05fd8ba94bb3c
salazar2356 <isasalazar2356@gmail.com>
2024-10-24 21:05:03 -0500

    Feature: Added scroll to History component

--- src/pages/IncomeAndIncrease/ExpensesAndIncome.tsx
+++ src/pages/IncomeAndIncrease/ExpensesAndIncome.tsx
@@ -2,12 +2,13 @@ import { ExpensePlanner } from "../../components/dashboardPageComponents/expense
 import { ExpensesGraphic } from "../../components/dashboardPageComponents/expensesAndIncomeScreenComponents/ExpensesGraphic/ExpensesGraphic";
 import { PieChartGraphic } from "../../components/dashboardPageComponents/expensesAndIncomeScreenComponents/PieChart/PieChartGraphic";
 import { History } from "../../components/dashboardPageComponents/expensesAndIncomeScreenComponents/History/History";
-
+import Nav from "../../components/Nav/Nav";
 
 export const ExpensesAndIncome = () => {
   return (
-    <div className='page'>
+    <div className='ExpensesAndIncome'>
         <h1>Income and Increase</h1>
+        <Nav/>
         <ExpensesGraphic />
         <PieChartGraphic />
         <ExpensePlanner />
