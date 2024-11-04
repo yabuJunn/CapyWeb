ff328ca48303f3cc59f57b18c040fffc2884f453
yabuJunn <juancho04reyes@gmail.com>
2024-11-03 19:19:12 -0500

    feat(feature): Created Savings History content
    Created the content of Savings History of the Savings Page, just visuals, not logic
    
    By: Juan David

--- src/pages/savingsPage/savingsPage.tsx
+++ src/pages/savingsPage/savingsPage.tsx
@@ -264,6 +264,19 @@ export const SavingsPage = () => {
 
                         <div id='savingsHistoryContainer'>
                             <h3>Savings History</h3>
+                            <div id='savingsHistoryItemsContainer'>
+                                <div className='savingsHistoryItem'>
+                                    <div className='savingsHistoryItemTitle'>
+                                        <img src={fireIconWhite} alt="" style={{ backgroundColor: '#2D18BF' }} />
+                                        <div className='savingsHistoryItemTitleText'>
+                                            <h4>My Party</h4>
+                                            <p>Savings per month: $50,000 cop</p>
+                                        </div>
+                                    </div>
+
+                                    <p className='savingsHistoryItemAmount'>$50,000</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
