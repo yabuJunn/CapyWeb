6b4a1e694106313e7e9fb131e49a26209c567fcc
yabuJunn <juancho04reyes@gmail.com>
2024-11-04 12:53:49 -0500

    feat(feature): Savings Page all painting with logic and mock data
    All content and components of the savings page is painting with mocked data and map logic
    
    By: Juan David

--- src/components/savingsPageComponents/SavingsHistory/SavingsHistory.tsx
+++ src/components/savingsPageComponents/SavingsHistory/SavingsHistory.tsx
@@ -1,24 +1,30 @@
 import './SavingsHistory.css'
 
-import fireIconWhite from '../../../assets/svg/fireIconWhite.svg'
+import { SavingHistoryItem } from '../SavingHistoryItem/SavingHistoryItem'
+import { savingType } from '../../../pages/savingsPage/savingsPage'
 
-export const SavingsHistory = () => {
-    return <>
-        <div id='savingsHistoryContainer'>
+interface SavingsHistoryProps {
+    savingsData: Array<savingType>
+}
+
+export const SavingsHistory = ({ savingsData }: SavingsHistoryProps) => {
+    return (
+        <div id="savingsHistoryContainer">
             <h3>Savings History</h3>
-            <div id='savingsHistoryItemsContainer'>
-                <div className='savingsHistoryItem'>
-                    <div className='savingsHistoryItemTitle'>
-                        <img src={fireIconWhite} alt="" style={{ backgroundColor: '#2D18BF' }} />
-                        <div className='savingsHistoryItemTitleText'>
-                            <h4>My Party</h4>
-                            <p>Savings per month: $50,000 cop</p>
-                        </div>
-                    </div>
-
-                    <p className='savingsHistoryItemAmount'>$50,000</p>
-                </div>
+            <div id="savingsHistoryItemsContainer">
+                {savingsData.map((savingItem) => (
+                    savingItem.savingHistory.map((savingItemHistoryItem) => (
+                        <SavingHistoryItem
+                            key={`${savingItem.savingName}-${savingItemHistoryItem.date}`} // Asegúrate de tener una clave única
+                            savingHistoryImage={savingItem.savingImage}
+                            savingHistoryColor={savingItem.savingColor}
+                            savingHistoryTitle={savingItem.savingName}
+                            savingHistoryDate={savingItemHistoryItem.date}
+                            savingHistoryDeposit={savingItemHistoryItem.deposit}
+                        />
+                    ))
+                ))}
             </div>
         </div>
-    </>
+    )
 }
--- src/pages/savingsPage/savingsPage.tsx
+++ src/pages/savingsPage/savingsPage.tsx
@@ -16,19 +16,52 @@ import KeyIconWhite from '../../assets/svg/KeyIconWhite.svg'
 //Savings Data
 
 const savingsData: Array<savingType> = [
-    { savingName: "Party", savingValue: 40, savingColor: "#2D18BF", savingPercentage: "40%", savingImage: fireIconWhite, monthlySaving: 50000, savingActualFee: 100000, savingTotalFee: 600000 },
-    { savingName: "Trip", savingValue: 28, savingColor: "#A8F25D", savingPercentage: "28%", savingImage: planetIconBlack, monthlySaving: 20000, savingActualFee: 40000, savingTotalFee: 480000 },
-    { savingName: "Motorbike", savingValue: 27, savingColor: "#F2622E", savingPercentage: "27%", savingImage: KeyIconWhite, monthlySaving: 100000, savingActualFee: 400000, savingTotalFee: 1000000 },
+    {
+        savingName: "Party", savingValue: 40, savingColor: "#2D18BF", savingPercentage: "40%", savingImage: fireIconWhite, monthlySaving: 50000, savingActualFee: 100000, savingTotalFee: 600000,
+        savingHistory: [
+            {
+                date: 'Miercoles 04 de Septiembre',
+                deposit: 50000
+            },
+            {
+                date: 'Domingo 04 de Agosto',
+                deposit: 50000
+            }
+        ]
+    },
+    {
+        savingName: "Trip", savingValue: 28, savingColor: "#A8F25D", savingPercentage: "28%", savingImage: planetIconBlack, monthlySaving: 20000, savingActualFee: 40000, savingTotalFee: 480000,
+        savingHistory: [
+            {
+                date: 'Lunes 02 de Septiembre',
+                deposit: 20000
+            },
+            {
+                date: 'Sabado 03 de Agosto',
+                deposit: 20000
+            },
+        ]
+    },
+    {
+        savingName: "Motorbike", savingValue: 27, savingColor: "#F2622E", savingPercentage: "27%", savingImage: KeyIconWhite, monthlySaving: 100000, savingActualFee: 400000, savingTotalFee: 1000000,
+        savingHistory: [
+            {
+                date: 'Miercoles 04 de Agosto',
+                deposit: 100000
+            },
+        ]
+    },
     {
         savingName: "Others", savingValue: 5, savingColor: "#C4C4C4", savingPercentage: "5%",
         savingImage: '',
         monthlySaving: 0,
         savingActualFee: 0,
-        savingTotalFee: 0
+        savingTotalFee: 0,
+        savingHistory: []
     }
 ]
 
-interface savingType {
+export interface savingType {
     savingName: string,
     savingValue: number,
     savingColor: string,
@@ -36,8 +69,15 @@ interface savingType {
     savingImage: string,
     monthlySaving: number,
     savingActualFee: number,
-    savingTotalFee: number
+    savingTotalFee: number,
+    savingHistory: Array<savingHistoryType>
 }
+
+export interface savingHistoryType {
+    date: string,
+    deposit: number
+}
+
 
 export const SavingsPage = () => {
     const chartData: Array<savingItemType> = []
@@ -88,7 +128,7 @@ export const SavingsPage = () => {
 
                         </div>
 
-                        <SavingsHistory></SavingsHistory>
+                        <SavingsHistory savingsData={savingsData}></SavingsHistory>
 
                     </div>
                 </div>
