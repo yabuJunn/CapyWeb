4bdaad92032beb439649208c289084c10a4210d8
shalom05 <salito0527@gmail.com>
2024-11-06 09:25:21 -0500

    style(feat):responsive was improved
    The responsiveness of this screen was improved, also taking into account the performance
    
    by:Salome Ruiz

--- src/components/RewardsPageComponents/SaverLevelTitleDisplay/SaverLevelTitleDisplay.tsx
+++ src/components/RewardsPageComponents/SaverLevelTitleDisplay/SaverLevelTitleDisplay.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import './SaverLevelTitleDisplay.css';
 import noviceSaverLevel from '../../../assets/desktop/png/noviceSaverLevel.png';
 
@@ -6,10 +7,25 @@ interface SaverLevelTitleDisplayProps {
 }
 
 export const SaverLevelTitleDisplay = ({ levelTitle }: SaverLevelTitleDisplayProps) => {
+  useEffect(() => {
+    const link = document.createElement('link');
+    link.rel = 'preload';
+    link.href = noviceSaverLevel;
+    link.as = 'image';
+    link.type = 'image/png';
+
+    document.head.appendChild(link);
+
+    return () => {
+      document.head.removeChild(link);
+    };
+  }, []);
+
   return (
-    <div id='SaverLevelTitleDisplayContainer'>
+    <div id="SaverLevelTitleDisplayContainer">
       <h3>{levelTitle}</h3>
       <img src={noviceSaverLevel} alt={`Nivel ${levelTitle} del ahorrador`} loading="eager" />
     </div>
   );
 };
+
