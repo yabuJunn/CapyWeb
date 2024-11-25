6701c8e0d36932b287d6b5111c14052ac44a029e
shalom05 <salito0527@gmail.com>
2024-11-24 23:41:10 -0500

    feat(feature): Created new function to register users according new data used on the redux

--- src/services/Firebase/FirestoreUsers.ts
+++ src/services/Firebase/FirestoreUsers.ts
@@ -3,6 +3,7 @@ import "firebase/firestore";
 
 import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
 import { app } from "./Firebase";
+import { saverLevelsNames } from "../../store/rewards/types";
 
 
 const db = getFirestore(app);
@@ -35,6 +36,34 @@ export const createUser = async (userUIDForm: string, nameForm: string, emailFor
         expenses: {},
         income: {},
         cards: {}
+    });
+
+    const docRef = doc(db, "users", userUIDForm);
+    const data = await getDoc(docRef);
+    return data.data()
+}
+
+export const createFinalUser = async (userUIDForm: string, nameForm: string, emailForm: string) => {
+    await setDoc(doc(db, "realUsers", userUIDForm), {
+        name: nameForm,
+        email: emailForm,
+        userUID: userUIDForm,
+        totalBalance: 0,
+        totalExpenses: 0,
+        totalIncome: 0,
+        totalSavings: 0,
+        saverLevel: {
+            saverLevelName: saverLevelsNames.level1,
+            userExpGained: 0,
+            accumulatedCapypoints: 0
+        },
+        realExpenses: [],
+        realIncomes: [],
+        plannedExpenses: [],
+        cards: [],
+        exchangeData: [],
+        missionsDatas: [],
+        savingsData: []
     });
 
     const docRef = doc(db, "users", userUIDForm);
