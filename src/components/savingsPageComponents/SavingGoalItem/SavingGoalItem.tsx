975c2d70eb1b09090bdf87289b5957f078cd06ca
yabuJunn <juancho04reyes@gmail.com>
2024-11-28 13:02:21 -0500

    feat(feature): Created edit saving goal modal
    By: Juan David

--- src/components/savingsPageComponents/SavingGoalItem/SavingGoalItem.tsx
+++ src/components/savingsPageComponents/SavingGoalItem/SavingGoalItem.tsx
@@ -1,6 +1,9 @@
 import './SavingGoalItem.css'
 
 import { Progress } from '../../../components/ui/progress'
+import { useState } from 'react'
+
+import xIconWhite from '../../../assets/desktop/svg/icons/xIconWhite.svg'
 
 export interface SavingGoalItemProps {
     goalImage: string,
@@ -12,29 +15,83 @@ export interface SavingGoalItemProps {
 }
 
 export const SavingGoalItem = ({ goalImage, goalImageColor, goalTitle, goalMonthlySaving, goalActualFee, goalTotalFee }: SavingGoalItemProps) => {
-    return <>
-        <div className='savingGoalItemContainer'>
-            <div className='savingGoalHeader'>
-                <div className='savingGoalTitle'>
-                    <img src={goalImage} alt="" style={{ backgroundColor: goalImageColor }} />
-                    <div className='savingGoalTitleText'>
-                        <h4>{goalTitle}</h4>
-                        <p>Savings per month: {goalMonthlySaving}</p>
+    const [editSavingModal, setEditSavingModal] = useState(false)
+
+    if (editSavingModal) {
+        return <>
+            <div id='savingGoalItemEditModalContainer'>
+                <div id='savingGoalItemEditModalContent'>
+                    <button id='savingGoalEditCloseModalButton' onClick={() => {
+                        setEditSavingModal(false)
+                    }}>
+                        <img src={xIconWhite} alt="close edit saving" />
+                    </button>
+
+                    <form action="" id="savingGoalItemEditModalContentForm">
+                        <h3>Edit Saving</h3>
+
+                        <div className="editGoalInputTextContainer">
+                            <label htmlFor="">Saving name</label>
+                            <input type="text" placeholder="Enter saving name" value={goalTitle} />
+                        </div>
+
+                        <div id='EditModalButtonsContainer'>
+                            <button id='addMonthlySavingButton'>Add Monthly Saving</button>
+                            <button id='deleteSavingButton'>Delete Saving</button>
+                        </div>
+                    </form>
+
+
+                </div>
+            </div>
+
+            <div className='savingGoalItemContainer'>
+                <div className='savingGoalHeader'>
+                    <div className='savingGoalTitle'>
+                        <img src={goalImage} alt="" style={{ backgroundColor: goalImageColor }} />
+                        <div className='savingGoalTitleText'>
+                            <h4>{goalTitle}</h4>
+                            <p>Savings per month: {goalMonthlySaving}</p>
+                        </div>
                     </div>
+
+                    <button>
+                        Edit
+                    </button>
                 </div>
 
-                <button>
-                    Edit
-                </button>
-            </div>
-
-            <div className='savingGoalGraphContainer'>
-                <div className='savingGoalGraphLabels'>
-                    <p>Actual: ${goalActualFee} cop</p>
-                    <p>Goal: ${goalTotalFee} cop</p>
+                <div className='savingGoalGraphContainer'>
+                    <div className='savingGoalGraphLabels'>
+                        <p>Actual: ${goalActualFee} cop</p>
+                        <p>Goal: ${goalTotalFee} cop</p>
+                    </div>
+                    <Progress value={(100 * goalActualFee) / goalTotalFee} className="w-[100%] h-[4vh]" style={{ border: '3px white solid' }}></Progress>
                 </div>
-                <Progress value={(100 * goalActualFee) / goalTotalFee} className="w-[100%] h-[4vh]" style={{ border: '3px white solid' }}></Progress>
             </div>
-        </div>
-    </>
+        </>
+    } else {
+        return <>
+            <div className='savingGoalItemContainer'>
+                <div className='savingGoalHeader'>
+                    <div className='savingGoalTitle'>
+                        <img src={goalImage} alt="" style={{ backgroundColor: goalImageColor }} />
+                        <div className='savingGoalTitleText'>
+                            <h4>{goalTitle}</h4>
+                            <p>Savings per month: {goalMonthlySaving}</p>
+                        </div>
+                    </div>
+
+                    <button onClick={() => { setEditSavingModal(true) }}>Edit</button>
+                </div>
+
+                <div className='savingGoalGraphContainer'>
+                    <div className='savingGoalGraphLabels'>
+                        <p>Actual: ${goalActualFee} cop</p>
+                        <p>Goal: ${goalTotalFee} cop</p>
+                    </div>
+                    <Progress value={(100 * goalActualFee) / goalTotalFee} className="w-[100%] h-[4vh]" style={{ border: '3px white solid' }}></Progress>
+                </div>
+            </div>
+        </>
+    }
 }
