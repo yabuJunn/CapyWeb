2c30e24ac54ceb945c04a1f8e30453d8bc1e1c8d
DANDIERU <1193080635@u.icesi.edu.co>
2024-11-28 00:42:24 -0500

    fix(feature) Create individual component for dinamic popup with messages

--- src/components/capyAssistantComponent/capyAssistantComponent.tsx
+++ src/components/capyAssistantComponent/capyAssistantComponent.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { CapyPopup } from "../capyPopupComponent/capyPopupComponent";
 import './capyAssistantComponent.css';
 
 export const CapyAssistant = () => {
@@ -16,15 +17,10 @@ export const CapyAssistant = () => {
                     alt="" 
                     className="assistant-img" 
                 />
-            </div>
+            </div>   
             {isPopupVisible && (
-                <div className="capy-popup">
-                    <button className="popup-btn">Give me a suggestion</button>
-                    <button className="popup-btn">Explain the metrics to me</button>
-                    <button className="popup-btn">Give me an idea</button>
-                    <button className="popup-btn">Customize Capy!</button>
-                </div>
-            )}
+                <CapyPopup></CapyPopup>
+            )}         
         </div>
     );
 };
